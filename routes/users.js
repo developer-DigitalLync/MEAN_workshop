f5d307954a4396575c7137b2de5fe84454182970
Naveen Vedala <Naveen@naveen23.local>
2018-02-09 13:25:17 +0530

    Changes

--- routes/users.js
+++ routes/users.js
@@ -41,39 +41,46 @@ router.post('/register', function(req, res, next) {
               html: "<a href="+link+"><b>Thank you For Registering click here Link to Set Password</b></a>"
             };
             transporter.sendMail(mailOptions, function (err, info) {
-              if(err)
-                console.log(err)
-              else
-                console.log(info);
+              if(err){
+               res.send({status:"error", message:"Something went wrong, please try again"});
+                user.findOneAndRemove({email:data.email});
+              }
+              else{
+                res.send({status:"success", message:"Registration success, Check your mail for setpassword"});
+              }
             });
           }
          })
         })
       })
-  res.send({data:"Registration success"});
+  
 });
 
 router.post('/password', (req, res)=>{
   let hash = bcryptjs.hashSync(req.body.password, 8);
-  user.findOneAndUpdate({email:req.body.email}, {$set: { password: hash }}, {new:true},(err, data)=>{
-    if(err) res.send("Error")
-    res.send({data:"Password set Successfully"});
+  console.log(hash)
+  user.findOneAndUpdate({email:req.body.email}, {$set:{ password: hash }}, {new:true},(err, data)=>{
+    if(err) res.send(err)
+      if(data){ res.send({status:"error", message:"Password set successfully"}); 
+  }else{
+    res.send({status:"error", message:"Something went wrong, Try again"})
+  }
   })
 })
 
 router.get('/', (req, res)=>{
-  res.send({data:"Workshop Application"})
+  res.send("Workshop Application")
 })
 
 router.get('/users', (req,res)=>{
     user.find({}).populate('work').populate('education').exec((err,data)=>{
-      res.send(data);
+      res.send({status:"success", message:data});
     })
 })
 
 router.get('/details', (req, res)=>{
   user.findById(req.query.id).populate('work').populate('education').exec((err, data)=>{
-    res.send(data);
+    res.send({status:"success", message:data});
   })
 })
 
@@ -82,12 +89,12 @@ router.post('/login', (req, res)=>{
     let compare = bcryptjs.compareSync(req.body.password, data.password);
     if(err) res.send({data:"Something went wrong while logging"});
     if(!data){
-      res.send({data:'No User Found'})
+      res.send({status:"error", message:'No User Found'})
     }else if(!compare){
-      res.send({data:"Invalid Credentials"})
+      res.send({status:"error", message:"Invalid Credentials"})
     }else{
       delete data["password"];
-      res.send(data);
+      res.send({status:"success", message:data});
     }
   })
 })
